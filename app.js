cc5086a8e75f40c9375105e8f70ba67bb9b81720
MukulChoudhary11 <125805364+MukulChoudhary11@users.noreply.github.com>
2023-02-23 00:18:52 +0530

    adding second file

---
+++ app.js
@@ -0,0 +1,7 @@
+var items =document.getElementsByClassName('list-group-item');
+items.style.fontWeight='bold';
+var items =document.getElementsByClassName('list-group-item');
+items[1].style.fontWeight='bold';
+'bold'
+items[0].style.backgroundColor='green';
+'green'
