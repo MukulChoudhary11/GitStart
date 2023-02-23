9c50a961d762f46903bfe8c152061d498f5d6a21
MukulChoudhary11 <125805364+MukulChoudhary11@users.noreply.github.com>
2023-02-23 09:09:48 +0530

    4th change

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ items[2].style.backgroundColor='grey';
 for(var i=0;i<=items.length;i++){
     items[i].style.fontWeight='bold';
 }
+
+//4th change
+var li=document.getElementsByTagName('li');
+li[1].textContent='Hello 2';
+li[1].style.fontWeight='bold';
+li[1].style.backdropColor='yellow';
+
+for(var i=0;i<li.length;i++){
+    items[i].style.backdropColor='#f4f4f4';
+}
