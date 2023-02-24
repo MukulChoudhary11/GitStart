b8a91a6d54297a98d61dfaa8ef6d13216339a736
MukulChoudhary11 <125805364+MukulChoudhary11@users.noreply.github.com>
2023-02-24 10:34:54 +0530

    5th change

--- app.js
+++ app.js
@@ -21,3 +21,9 @@ li[1].style.backdropColor='yellow';
 for(var i=0;i<li.length;i++){
     items[i].style.backdropColor='#f4f4f4';
 }
+//5th change
+var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color='green';
+var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.display='none';
+
