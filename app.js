88c0ab3c31eef787debe7988245f16ced19faf9b
MukulChoudhary11 <125805364+MukulChoudhary11@users.noreply.github.com>
2023-02-24 11:15:22 +0530

    5th change

--- app.js
+++ app.js
@@ -27,3 +27,9 @@ seconditem.style.color='green';
 var thirditem=document.querySelector('.list-group-item:nth-child(3)');
 thirditem.style.display='none';
 
+var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color='green';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
